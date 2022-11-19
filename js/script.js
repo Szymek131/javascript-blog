314c8ccf5c7359df85af92d66d512b53e3e67754
Szymon Wiktor <sz.wiktor131@gmail.com>
2022-11-19 18:51:38 +0100

    Add generateTitleLinks function in JS

--- js/script.js
+++ js/script.js
@@ -31,37 +31,23 @@ const titleClickHandler = function(event){
 
   function generateTitleLinks() {
     
-    /* [DONE] remove contents of titleList */
-
     document.querySelector(optTitleListSelector).innerHTML = '';
 
-    /* for each article */
 
-        const articles = document.querySelectorAll(optArticleSelector);
+    const articles = document.querySelectorAll(optArticleSelector);
 
-        for(let article of articles){
+    for(let article of articles){
 
-        /* get the article id */
         const articleId = article.getAttribute('id');
-        console.log(articleId);
-        /* find the title element */
         const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-        console.log(articleTitle);
-
-        /* get the title from the title element */
-
-        /* create HTML of the link */
-
         const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-        console.log(linkHTML);
-
-        /* insert link into titleList */
-
         const position = document.querySelector('.titles')
+        
         position.insertAdjacentHTML('beforeend', linkHTML);
     }
+    
     const links = document.querySelectorAll('.titles a');
-    console.log(links);
+
 
     for(let link of links){
         link.addEventListener('click', titleClickHandler);
